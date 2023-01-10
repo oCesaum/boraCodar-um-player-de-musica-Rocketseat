b708a4b28bb4468c1b230eeb288d53e4d55dc31e
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-01-09 21:03:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,94 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-container">
+        <div className="left-container">
+          <img
+            className="left-container-music-img"
+            src="/src/assets/image-189.66.png"
+            alt="Music image"
+          />
+          <div className="music-info-container">
+            <h1>Acorda Devinho</h1>
+            <p>Banda Rocketseat</p>
+          </div>
+          <div className="left-play-settings">
+            <img src="/src/assets/play-back.svg" alt="Play back button" />
+            <img src="/src/assets/play.svg" alt="Play button" />
+            <img src="/src/assets/play-forward.svg" alt="Play forward button" />
+          </div>
+          <div className="left-play-details">
+            <div className="playbar-container">
+              <div className="left-playbar"></div>
+              <div className="left-listened-playbar"></div>
+            </div>
+            <div className="playbar-timer">
+              <p>03:20</p>
+              <p>00:12</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-container">
+          <div className="right-top-container">
+            <div className="music-details-container">
+              <img src="/src/assets/image-84.03.png" alt="Music image" />
+              <div className="music-info-container">
+                <h1>Acorda Devinho</h1>
+                <p>Banda Rocketseat</p>
+              </div>
+            </div>
+            <div className="right-play-settings">
+              <img src="/src/assets/play-back.svg" alt="Play back button" />
+              <img
+                className="right-top-play-button"
+                src="/src/assets/play.svg"
+                alt="Play button"
+              />
+              <img
+                src="/src/assets/play-forward.svg"
+                alt="Play forward button"
+              />
+            </div>
+            <div className="right-play-details">
+              <div className="playbar-container">
+                <div className="right-playbar"></div>
+                <div className="right-listened-playbar"></div>
+              </div>
+              <div className="playbar-timer">
+                <p>03:20</p>
+                <p>00:12</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="right-bottom-container">
+            <div className="music-details-container">
+              <img src="/src/assets/image-84.03.png" alt="Music image" />
+              <div className="music-info-container">
+                <h1>Acorda Devinho</h1>
+                <p>Banda Rocketseat</p>
+              </div>
+            </div>
+            <div className="right-play-settings">
+              <img src="/src/assets/play-back.svg" alt="Play back button" />
+              <img
+                className="right-top-play-button"
+                src="/src/assets/play.svg"
+                alt="Play button"
+              />
+              <img
+                src="/src/assets/play-forward.svg"
+                alt="Play forward button"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
